dacad0843dc96af06d7d2bad9a23b3942d91bfb0
Apollo Akora <apolloakora@gmail.com>
2019-08-20 15:22:18 +0100

    renaming the ecosystem and timestamp variables

--- ecs.profile-variables.tf
+++ ecs.profile-variables.tf
@@ -17,7 +17,7 @@ variable in_policy_stmts {
 ### in_ecosystem_name ###
 ### ################# ###
 
-variable in_ecosystem_name {
+variable in_ecosystem {
 
     description = "Creational stamp binding all infrastructure components created on behalf of this ecosystem instance."
 }
@@ -27,7 +27,7 @@ variable in_ecosystem_name {
 ### in_tag_timestamp ###
 ### ################ ###
 
-variable in_tag_timestamp {
+variable in_timestamp {
 
     description = "A timestamp for resource tags in the format ymmdd-hhmm like 80911-1435"
 }
